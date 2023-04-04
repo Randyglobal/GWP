a4b6e0d9e5d896a3eb0fa67837f1ea032067db76
Randyglobal <randyitoe2255@gmail.com>
2023-04-04 14:31:11 -0700

    Ecommerce site

---
+++ index.js
@@ -0,0 +1,16 @@
+let trigger = document.getElementById('Samsung');
+
+let section = document.querySelector('.trigger');
+
+// let accesseries = document.querySelector('.accessories');
+
+section.style.display = 'none';
+accesseries.style.display = 'none';
+
+trigger.addEventListener('click', function(){
+    if (section.style.display == 'none') {
+        section.style.display = 'flex';
+    }else{
+
+    }
+});
